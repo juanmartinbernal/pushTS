52f29552c820bb81db8cf875fac68575b6d676f6
bernaljuan <jmartinb@seidor.es>
2020-04-14 12:04:47 +0200

    se definen los métodos para enviar mensajes a uno o varios destinatarios

--- firebaseadmin.ts
+++ firebaseadmin.ts
@@ -11,47 +11,87 @@ admin.initializeApp({
 var device_token =
   'eSQhdyQ7UuU:APA91bGlYdyr-af80L_EnONQEFJRgwtS6Hi06tjhTcMnsxdEHYTuXR0Rb8TyXWu_cSnnhB5mCFHLKyQnhq9wKIQ1HCHnGiPHrmjsSQoPVPGiETIeukzIS4N09ByysYF7xcIiRIEDs128';
 
-var registrationToken = device_token;
+var registrationTokens = [device_token, device_token];
+export class MessageFCM {
+  public sendMessage(
+    title: string,
+    icon: string,
+    body: string,
+    registrationToken: string
+  ) {
+    var message = {
+      notification: {
+        title: `${title}`,
+        body: `${body}`,
+      },
+      data: {
+        score: '850',
+        time: '2:45',
+      },
+      token: registrationToken,
+    };
 
-var message = {
-  notification: {
-    title: 'Title juan of your push notification',
-    body: 'Body of your push notification khbhjbhbsssssss',
-  },
-  data: {
-    score: '850',
-    time: '2:45',
-  },
-  token: registrationToken,
-};
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    if (registrationToken != null && registrationToken.length > 0) {
+      admin
+        .messaging()
+        .send(message)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+          console.log('Error sending message:', error);
+        });
+    } else {
+      console.log(' Debes haber destinantarios para el mensaje');
+    }
+  }
 
-// Send a message to the device corresponding to the provided
-// registration token.
-admin
-  .messaging()
-  .send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
-  });
+  /**
+   *  //envio a varios dispositivos
+    // Create a list containing up to 500 registration tokens.
+    // These registration tokens come from the client FCM SDKs.
+    const registrationTokens = [
+      'YOUR_REGISTRATION_TOKEN_1',
+      // …
+      'YOUR_REGISTRATION_TOKEN_N',
+    ];
+   * @param title
+   * @param icon 
+   * @param body 
+   * @param registrationTokens 
+   */
+  public sendMultipleMessage(
+    title: string,
+    icon: string,
+    body: string,
+    registrationTokens: string[]
+  ) {
+    const message = {
+      notification: {
+        title: `${title}`,
+        body: `${body}`,
+      },
+      data: { score: '850', time: '2:45' },
+      tokens: registrationTokens,
+    };
 
-//envio a varios dispositivos
-// Create a list containing up to 500 registration tokens.
-// These registration tokens come from the client FCM SDKs.
-/*const registrationTokens = [
-  'YOUR_REGISTRATION_TOKEN_1',
-  // …
-  'YOUR_REGISTRATION_TOKEN_N',
-];
+    if (registrationTokens != null && registrationTokens.length > 0) {
+      admin
+        .messaging()
+        .sendMulticast(message)
+        .then((response) => {
+          console.log(
+            response.successCount + ' messages were sent successfully'
+          );
+        });
+    } else {
+      console.log(' Debes haber destinantarios para el mensaje');
+    }
+  }
+} //end class
 
-const message = {
-  data: {score: '850', time: '2:45'},
-  tokens: registrationTokens,
-}
-
-admin.messaging().sendMulticast(message)
-  .then((response) => {
-    console.log(response.successCount + ' messages were sent successfully');*/
+let message = new MessageFCM();
+message.sendMultipleMessage('Title2', 'Icon2', 'body', registrationTokens);
